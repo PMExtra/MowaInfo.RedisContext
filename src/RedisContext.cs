cba56e2862a1cf239be8b49da5138bb46cbdb4dd
PM Extra <pm@jubeat.net>
2017-07-15 20:50:26 +0800

    Initial commit.

---
+++ src/RedisContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using StackExchange.Redis;
+
+namespace MowaInfo.RedisContext
+{
+    public class RedisContext
+    {
+        private readonly ConnectionMultiplexer _connection;
+
+        public RedisContext(HostString host)
+        {
+            var addresses = Dns.GetHostAddressesAsync(host.Host).Result.Select(addr => addr.MapToIPv4());
+            var configuration = host.Port == null
+                ? string.Join(",", addresses)
+                : string.Join(",", addresses.Select(addr => $"{addr}:{host.Port}"));
+            _connection = ConnectionMultiplexer.Connect(configuration);
+        }
+
+        protected IDatabase GetDatabase(int db = -1)
+        {
+            return _connection.GetDatabase(db);
+        }
+
+        protected ISubscriber GetSubscriber()
+        {
+            return _connection.GetSubscriber();
+        }
+    }
+}
