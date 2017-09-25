5a8ae4a42aab7d561b908600fb5b2f66cf08ef83
PM Extra <pm@jubeat.net>
2017-09-25 23:10:58 +0800

    IRedisContextBuilder

--- src/MowaInfo.RedisContext/DependencyInjection/IRedisContextBuilder.cs
+++ src/MowaInfo.RedisContext/DependencyInjection/IRedisContextBuilder.cs
@@ -1,6 +1,12 @@
-﻿namespace MowaInfo.RedisContext.DependencyInjection
+﻿using MowaInfo.RedisContext.Core;
+
+namespace MowaInfo.RedisContext.DependencyInjection
 {
     public interface IRedisContextBuilder
     {
+        IRedisContextBuilder AddObserver<T>() where T : RedisObserver, new();
+        IRedisContextBuilder AddObserver(RedisObserver observer);
+        IRedisContextBuilder AddPublisher<T>() where T : RedisPublisher, new();
+        IRedisContextBuilder AddPublisher(RedisPublisher publisher);
     }
 }
