b342d6e67af9b8f27f5c4cb7e7feae971855bcc7
PM Extra <pm@jubeat.net>
2017-11-30 19:31:34 +0800

    Add tests.

---
+++ test/MowaInfo.RedisContext.Tests/TestRedisContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using MowaInfo.RedisContext.Annotations;
+using StackExchange.Redis;
+
+namespace MowaInfo.RedisContext.Tests
+{
+    public class TestRedisContext : Core.RedisContext
+    {
+        public TestRedisContext(HostString host) : base(host)
+        {
+        }
+
+        public TestRedisContext(ConfigurationOptions configuration) : base(configuration)
+        {
+        }
+
+        [GetDatabase(1)]
+        public TestRedisDatabase TestRedisDatabase { get; set; }
+    }
+}
---
+++ test/MowaInfo.RedisContext.Tests/TestRedisDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using MowaInfo.RedisContext.Core;
+using StackExchange.Redis;
+
+namespace MowaInfo.RedisContext.Tests
+{
+    public class TestRedisDatabase : RedisDatabase
+    {
+        public async Task<RedisValue> TestGetAsync(string key)
+        {
+            return await Database.StringGetAsync(key);
+        }
+
+        public Task<bool> TestSetAsync(string key, string value)
+        {
+            return Database.StringSetAsync(key, value);
+        }
+    }
+}
